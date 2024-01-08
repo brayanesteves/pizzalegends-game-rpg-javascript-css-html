989246ebaad8188f01fbdfb76c7bf49f57db8c02
Brayan Esteves <brayan.esteves93@gmail.com>
2024-01-08 18:19:39 -0400

    Adding Game Objects [08/01/2024]

--- src/public/assets/js/overworld.js
+++ src/public/assets/js/overworld.js
@@ -19,43 +19,22 @@ class Overworld {
         };
         image.src = "/src/public/assets/img/png/maps/DemoLower.png";
 
-        const x    = 5;
-        const y    = 6;
+        // Place some game object!
+        const hero = new GameObject({
+            x:5,
+            y:6,
+        });
 
-        const shadow = new Image();
-        shadow.onload = () => {
-            this.ctx.drawImage(
-                shadow, 
-                0,  // Left cut.
-                0,  // Top cut.
-                32, // Width of cut.
-                32, // Height of cut.
-                x * 16 - 8,
-                y * 16 - 18,
-                32,
-                32
+        const npc1 = new GameObject({
+              x:7,
+              y:9,
+            src:"/src/public/assets/img/png/characters/people/npc1.png"
+        });
 
-            );
-        };
-        shadow.src = "/src/public/assets/img/png/characters/shadow.png";
-
-        const hero = new Image();
-
-        hero.onload = () => {
-            this.ctx.drawImage(
-                hero, 
-                0,  // Left cut.
-                0,  // Top cut.
-                32, // Width of cut.
-                32, // Height of cut.
-                x * 16 - 8,
-                y * 16 - 18,
-                32,
-                32
-
-            );
-        };
-        hero.src = "/src/public/assets/img/png/characters/people/hero.png";
+        setTimeout(() => {
+            hero.sprite.draw(this.ctx);
+            npc1.sprite.draw(this.ctx);
+        }, 200);
 
     }
 }
