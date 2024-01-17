70cd8e9ff132245e3b9a8f0825664be22717c4d6
Brayan Esteves <brayan.esteves93@gmail.com>
2024-01-16 21:59:00 -0400

    Layer[Content] - pizza.js & images pizzas and icons [16/01/2024]

---
+++ src/public/assets/js/Content/pizza.js
@@ -0,0 +1,28 @@
+window.PizzaTypes = {
+    normal:"normal",
+     spicy:"spicy",
+    veggie:"veggie",
+     fungi:"fungi",
+     chill:"chill",
+};
+
+window.Pizzas = {
+    "s001": {
+        name:"Slice Samurai",
+        type:PizzaTypes.spicy,
+         src:"./src/public/assets/img/png/characters/pizzas/s001.png",
+        icon:"./src/public/assets/img/png/icons/spicy.png",
+    },
+    "v001": {
+        name:"Slice Samurai",
+        type:PizzaTypes.veggie,
+         src:"./src/public/assets/img/png/characters/pizzas/v001.png",
+        icon:"./src/public/assets/img/png/icons/veggie.png",
+    },
+    "f001": {
+        name:"Slice Samurai",
+        type:PizzaTypes.fungi,
+         src:"./src/public/assets/img/png/characters/pizzas/f001.png",
+        icon:"./src/public/assets/img/png/icons/fungi.png",
+    },
+};
